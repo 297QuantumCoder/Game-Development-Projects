5f1f0203d4d455864326655d2a9840c09df0a4d0
Umesh <brijbiharisingh1971@outlook.com>
2024-03-24 09:51:10 +0530

    Game Completed

--- WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/MainWindow.xaml.cs
@@ -15,15 +15,37 @@ using System.Windows.Shapes;
 
 namespace WpfApp2
 {
+    using System.Windows.Threading;
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int matchesFound;
+        int tenthOfSecondsElapsed ;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthOfSecondsElapsed ++;
+            timerTextBlock.Text = (tenthOfSecondsElapsed / 10F).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timerTextBlock.Text = timerTextBlock.Text + " - PLay Again ? ";
+            }
+                
+
+
         }
 
         private void SetUpGame()
@@ -44,11 +66,20 @@ namespace WpfApp2
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index]; 
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if(textBlock.Name != "timerTextBlock")
+                {
+                    textBlock.Visibility = Visibility.Visible;
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+
+            timer.Start();
+            tenthOfSecondsElapsed = 0;
+            matchesFound = 0;
+
         }
 
         TextBlock lastClickedTextBlock;
@@ -66,6 +97,7 @@ namespace WpfApp2
 
             else if(lastClickedTextBlock.Text == currentTextBlock.Text)
             {
+                matchesFound++;
                 findingMatch = false;
                 currentTextBlock.Visibility = Visibility.Hidden;
             }
@@ -78,6 +110,11 @@ namespace WpfApp2
 
         }
 
-      
+        private void timerTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+                SetUpGame();
+
+        }
     }
 }
